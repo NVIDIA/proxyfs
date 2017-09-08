83a4d921f232c98f8b15459204462312dd7a3992
Balaji Rao <balaji@swiftstack.com>
2017-09-07 17:40:51 -0700

    Removed conditional def for logging in jrpcclient.

--- jrpcclient/proxyfs_api.c
+++ jrpcclient/proxyfs_api.c
@@ -2866,11 +2866,7 @@ int proxyfs_write_recv(void*   in_request_id,
 #endif
 
 // Flag to control debug prints. Defaulted to on for now.
-#ifdef ENABLE_LOGGING
-int debug_flag = 1;
-#else
 int debug_flag = 0;
-#endif
 
 void proxyfs_set_verbose()
 {
