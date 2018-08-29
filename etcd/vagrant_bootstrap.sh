a9fd84a30195eb0e774286efa385dfb22f84cd87
Bob Schatz <bschatz@swiftstack.com>
2018-08-29 16:56:30 -0700

    Add proper path for /vagrant, install tools and unit tests
    Set /vagrant to be the same as SAIO.
    
    Install some of the same tools as SAIO (not gcc yet).
    
    Add unit test to test basic Get()/Put() and Txn()

--- etcd/vagrant_bootstrap.sh
+++ etcd/vagrant_bootstrap.sh
@@ -5,26 +5,57 @@ set -e
 yum -y update
 yum -y install ansible
 
+# Install yum-utils to deal with yum repos
+
+yum -y install yum-utils
+
+# Disable generic CentOS 7 repos
+
+yum-config-manager --disable CentOS-Base
+yum-config-manager --disable CentOS-CR
+yum-config-manager --disable CentOS-Debuginfo
+yum-config-manager --disable CentOS-fasttrack
+yum-config-manager --disable CentOS-Media
+yum-config-manager --disable CentOS-Sources
+yum-config-manager --disable CentOS-Vault
+
+rm -rf /etc/yum.repos.d/CentOS-Base.repo
+rm -rf /etc/yum.repos.d/CentOS-CR.repo
+rm -rf /etc/yum.repos.d/CentOS-Debuginfo.repo
+rm -rf /etc/yum.repos.d/CentOS-fasttrack.repo
+rm -rf /etc/yum.repos.d/CentOS-Media.repo
+rm -rf /etc/yum.repos.d/CentOS-Sources.repo
+rm -rf /etc/yum.repos.d/CentOS-Vault.repo
+
+# Add and enable CentOS 7.4 repos
+
+yum-config-manager --add-repo http://vault.centos.org/centos/7.4.1708/os/x86_64/
+yum-config-manager --add-repo http://vault.centos.org/centos/7.4.1708/updates/x86_64/
+yum-config-manager --add-repo http://vault.centos.org/centos/7.4.1708/extras/x86_64/
+yum-config-manager --add-repo http://vault.centos.org/centos/7.4.1708/centosplus/x86_64/
+yum-config-manager --enable vault.centos.org_centos_7.4.1708_os_x86_64_
+yum-config-manager --enable vault.centos.org_centos_7.4.1708_updates_x86_64_
+yum-config-manager --enable vault.centos.org_centos_7.4.1708_extras_x86_64_
+yum-config-manager --enable vault.centos.org_centos_7.4.1708_centosplus_x86_64_
+
+yum clean all
+
 # Install tools needed above what's in a minimal base box
 
 yum -y install wget git nfs-utils vim
 
 # Install Golang
-
-yum -y --disableexcludes=all install gcc
 cd /tmp
 wget -q https://dl.google.com/go/go1.10.linux-amd64.tar.gz
 tar -C /usr/local -xf go1.10.linux-amd64.tar.gz
 rm go1.10.linux-amd64.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~vagrant/.bash_profile
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~root/.bash_profile
-echo "export PATH=\$PATH:/usr/local/go/bin" >> ~root/.bashrc
 
 # Patch Golang's GDB runtime plug-in
 
 mv /usr/local/go/src/runtime/runtime-gdb.py /usr/local/go/src/runtime/runtime-gdb.py_ORIGINAL
-#TODO - cp /vagrant/src/github.com/swiftstack/ProxyFS/saio/usr/local/go/src/runtime/runtime-gdb.py /usr/local/go/src/runtime/.
-cp /vagrant/saio/usr/local/go/src/runtime/runtime-gdb.py /usr/local/go/src/runtime/.
+cp /vagrant/src/github.com/swiftstack/ProxyFS/saio/usr/local/go/src/runtime/runtime-gdb.py /usr/local/go/src/runtime/.
 
 # Install GDB and enable above Golang GDB runtime plug-in as well as other niceties
 
@@ -53,3 +84,25 @@ echo "export GOPATH=/vagrant" >> ~vagrant/.bash_profile
 echo "export PATH=\$PATH:\$GOPATH/bin" >> ~vagrant/.bash_profile
 echo "alias cdpfs=\"cd \$GOPATH/src/github.com/swiftstack/ProxyFS\"" >> ~vagrant/.bash_profile
 echo "user_allow_other" >> /etc/fuse.conf
+
+# Install wireshark
+
+yum -y install wireshark-gnome \
+               xorg-x11-fonts-Type1 \
+               xorg-x11-xauth \
+               xeyes
+echo "X11Forwarding yes" >> /etc/sysconfig/sshd
+systemctl restart sshd
+usermod -aG wireshark vagrant
+
+# Install benchmark support tools
+
+##yum -y install atop-2.3.0-8.el7 bc fio gawk
+
+# Install ssh helper
+
+yum -y install sshpass-1.06-2.el7
+
+# Install dstat
+
+yum -y install dstat
