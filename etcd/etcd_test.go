4919a05d5ff3b1a11b4890049a42e9bdbd901801
Bob Schatz <bschatz@swiftstack.com>
2018-09-10 13:32:23 -0700

    Add unit test for HB testing

--- etcd/etcd_test.go
+++ etcd/etcd_test.go
@@ -50,6 +50,7 @@ func TestEtcdAPI(t *testing.T) {
 	testBasicPutGet(t)
 	testBasicTxn(t)
 	testTxnWatcher(t)
+	testTxnHb(t)
 }
 
 // Test basic etcd API based on this link:
@@ -148,9 +149,26 @@ func watcher(t *testing.T, cli *clientv3.Client, key string, expectedValue strin
 	assert := assert.New(t)
 	wch1 := cli.Watch(context.Background(), key)
 	for wresp1 := range wch1 {
-		for _, ev := range wresp1.Events {
-			assert.Equal(expectedValue, string(ev.Kv.Value),
+		for _, e := range wresp1.Events {
+			assert.Equal(expectedValue, string(e.Kv.Value),
 				"watcher saw different value than expected")
+
+			// Handle heartbeat test case
+			if string(e.Kv.Key) == "TESTHB" {
+
+				// Get current time
+				currentTime := time.Now().UTC()
+
+				// Unmarshal time HB sent
+				var sentTime time.Time
+				err := sentTime.UnmarshalText(e.Kv.Value)
+				assert.Nil(err, "UnmarshalText() failed with HB")
+
+				// Current time must be at least 500 milliseconds later
+				testTime := sentTime
+				testTime.Add(500 * time.Millisecond)
+				assert.True(currentTime.After(testTime), "testTime !> 500 milliseconds")
+			}
 		}
 
 		// The watcher has received it's event and will now return
@@ -244,6 +262,72 @@ func testTxnWatcher(t *testing.T) {
 	).Else(
 		clientv3.OpPut(testKey1, "FailedTestKey1"),
 		clientv3.OpPut(testKey2, "FailedTestKey2"),
+	).Commit()
+	cancel()
+	if err != nil {
+		assert.Nil(err, "kvc.Txn() returned err")
+	}
+
+	// Wait for watchers to get events before exiting test
+	finishWG.Wait()
+}
+
+// Test sending heartbeat in txn() and watching for heartbeat to arrive.
+// per key updated.
+func testTxnHb(t *testing.T) {
+	var (
+		testHb   = "TESTHB"
+		startWG  sync.WaitGroup // Used to sync start of watcher
+		finishWG sync.WaitGroup // Used to block until watcher sees event
+	)
+	assert := assert.New(t)
+
+	testHbData, errTime := time.Now().UTC().MarshalText()
+	time.Sleep(1 * time.Second)
+	assert.Nil(errTime, "time.Now.UTC().MarshalText() returned err")
+
+	// Create an etcd client - our current etcd setup does not listen on
+	// localhost.  Therefore, we pass the IP addresses used by etcd.
+	cli, err := clientv3.New(clientv3.Config{Endpoints: []string{"192.168.60.10:2379",
+		"192.168.60.11:2379", "192.168.60.12:2379"}, DialTimeout: 2 * time.Second})
+	if err != nil {
+		assert.Nil(err, "clientv3.New() returned err")
+		// handle error!
+	}
+	defer cli.Close()
+
+	kvc := clientv3.NewKV(cli)
+
+	// Reset the test keys to be used
+	m := make(map[string]string)
+	m[testHb] = string(testHbData)
+	resetKeys(t, cli, m)
+
+	// Create watchers for each key
+	startWatchers(t, cli, m, &startWG, &finishWG)
+
+	// NOTE: "etcd does not ensure linearizability for watch operations. Users are
+	// expected to verify the revision of watch responses to ensure correct ordering."
+	// TODO - figure out what this means
+	// TODO - review use of WithRequireLeader.  Seems important for a subcluster to
+	//     recognize partitioned out of cluster.
+
+	// Send HB and guarantee get watch event for HB
+	finishWG.Add(len(m))
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	_, err = kvc.Txn(ctx).
+
+		// txn value comparisons are lexical
+		If(
+			clientv3.Compare(clientv3.Value(testHb), "=", ""),
+
+		// the "Then" runs, since "" = "" for both keys
+		).Then(
+		clientv3.OpPut(testHb, string(testHbData)),
+
+	// the "Else" does not run
+	).Else(
+		clientv3.OpPut(testHb, "FailedTestHb"),
 	).Commit()
 	cancel()
 	if err != nil {
