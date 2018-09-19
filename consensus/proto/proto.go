557714a3216bbd179a701ca298b0dc48a7a134a6
Bob Schatz <bschatz@swiftstack.com>
2018-09-19 10:18:16 -0700

    Add infinite loop to prototype to show daemon running
    Add loop to prototype to keep daemon up to simulate proxyfsd running.
    Otherwise, it will come up and leave...

--- consensus/proto/proto.go
+++ consensus/proto/proto.go
@@ -7,6 +7,10 @@ import (
 	"time"
 )
 
+var (
+	dummyBool = false
+)
+
 func main() {
 	endpoints := []string{"192.168.60.10:2379", "192.168.60.11:2379", "192.168.60.12:2379"}
 
@@ -18,6 +22,19 @@ func main() {
 		os.Exit(-1)
 	}
 
+	// Simulate proxyfsd being a long running daemon
+	// by looping here.
+	for {
+		time.Sleep(5 * time.Second)
+
+		// This code is never hit in this prototype.
+		// Just eliminates a VS warning.
+		if dummyBool {
+			break
+		}
+	}
+
+	// NOTE: Currently this code is never hit
 	// Unregister from the etcd cluster
 	cs.Unregister()
 }
