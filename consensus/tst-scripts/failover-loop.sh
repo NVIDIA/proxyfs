16e0ebc2d9993f8b5c970e9d0ae42947dd7225d8
Bob Schatz <bschatz@swiftstack.com>
2018-10-15 15:12:42 -0700

    Simple script to loop failing over VGs

---
+++ consensus/tst-scripts/failover-loop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x 
+
+i="myTestVg1"
+n=etcd0
+#for n in etcd0 etcd1 etcd2
+while :
+do
+	sleep 120
+	./hacli offline -vg $i
+	./hacli online -vg $i -node $n
+
+	if [ X"$i" ==  X"myTestVg1" ]
+	then
+		i="myTestVg2"
+	else
+		i="myTestVg1"
+	fi
+
+	if [ X"$n" ==  X"etcd0" ]
+	then
+		n="etcd1"
+	else
+		if [ X"$n" ==  X"etcd1" ]
+		then
+			n="etcd2"
+		else
+			n="etcd0"
+		fi
+	fi
+done
