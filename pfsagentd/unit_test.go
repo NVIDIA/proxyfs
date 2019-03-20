f89663edaa7e59de317aa07a83c92ecce9bde3cc
Ed McClanahan <ed@swiftstack.com>
2019-03-20 15:25:36 -0700

    Validate GET /info response
    TODO:
      Exercise PUT of a Container
      Exercise PUT of an Object
      Exercise Ranged GET of an Object
      Exercise JSON RPC Ping

--- pfsagentd/unit_test.go
+++ pfsagentd/unit_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -10,13 +11,36 @@ import (
 	"github.com/swiftstack/ProxyFS/jrpcfs"
 )
 
+const (
+	testMaxAccountNameLength   = 256
+	testMaxContainerNameLength = 512
+	testMaxObjectNameLength    = 1024
+	testAccountListingLimit    = 10000
+	testContainerListingLimit  = 20000
+)
+
+type testSwiftInfoInnerStruct struct {
+	MaxAccountNameLength   uint64 `json:"max_account_name_length"`
+	MaxContainerNameLength uint64 `json:"max_container_name_length"`
+	MaxObjectNameLength    uint64 `json:"max_object_name_length"`
+	AccountListingLimit    uint64 `json:"account_listing_limit"`
+	ContainerListingLimit  uint64 `json:"container_listing_limit"`
+}
+
+type testSwiftInfoOuterStruct struct {
+	Swift testSwiftInfoInnerStruct `json:"swift"`
+}
+
 func TestSwiftProxyEmulation(t *testing.T) {
 	var (
+		authRequest  *http.Request
+		authResponse *http.Response
 		authURL      string
 		err          error
 		infoBuf      []byte
 		infoResponse *http.Response
 		infoURL      string
+		swiftInfo    *testSwiftInfoOuterStruct
 	)
 
 	testSetup(t)
@@ -41,11 +65,51 @@ func TestSwiftProxyEmulation(t *testing.T) {
 		t.Fatalf("GET /info returned uncloseable Body: %v", err)
 	}
 
-	t.Logf("GET /info returned: %v", string(infoBuf[:]))
+	swiftInfo = &testSwiftInfoOuterStruct{}
+
+	err = json.Unmarshal(infoBuf, swiftInfo)
+	if nil != err {
+		t.Fatalf("GET /info returned unparseable Body: %v", err)
+	}
+
+	if (testMaxAccountNameLength != swiftInfo.Swift.MaxAccountNameLength) ||
+		(testMaxContainerNameLength != swiftInfo.Swift.MaxContainerNameLength) ||
+		(testMaxObjectNameLength != swiftInfo.Swift.MaxObjectNameLength) ||
+		(testAccountListingLimit != swiftInfo.Swift.AccountListingLimit) ||
+		(testContainerListingLimit != swiftInfo.Swift.ContainerListingLimit) {
+		t.Fatalf("GET /info returned unexpected swiftInfo")
+	}
 
 	authURL = strings.Join(append(strings.Split(globals.swiftAccountURL, "/")[:3], "auth", "v1.0"), "/")
 
-	fmt.Println("authURL ==", authURL) // TODO: Exercise AUTH
+	authRequest, err = http.NewRequest("GET", authURL, nil)
+	if nil != err {
+		t.Fatalf("Creating GET /auth/v1.0 http.Request failed: %v", err)
+	}
+
+	authRequest.Header.Add("X-Auth-User", globals.config.SwiftAuthUser)
+	authRequest.Header.Add("X-Auth-Key", globals.config.SwiftAuthKey)
+
+	authResponse, err = testSwiftProxyEmulatorGlobals.httpClient.Do(authRequest)
+	if nil != err {
+		t.Fatalf("GET /auth/v1.0 failed: %v", err)
+	}
+	if http.StatusOK != authResponse.StatusCode {
+		t.Fatalf("GET /auth/v1.0 returned bad status: %v (%v)", authResponse.Status, authResponse.StatusCode)
+	}
+
+	err = authResponse.Body.Close()
+	if nil != err {
+		t.Fatalf("GET /auth/v1.0 returned uncloseable Body: %v", err)
+	}
+
+	if testAuthToken != authResponse.Header.Get("X-Auth-Token") {
+		t.Fatalf("GET /auth/v1.0 returned incorrect X-Auth-Token")
+	}
+
+	if "http://"+testSwiftProxyAddr+"/v1/"+globals.config.SwiftAccountName != authResponse.Header.Get("X-Storage-Url") {
+		t.Fatalf("GET /auth/v1.0 returned incorrect X-Storage-Url")
+	}
 
 	// TODO: Exercise PUT of a Container
 
