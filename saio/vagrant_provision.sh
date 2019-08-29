0e20a84b1704ba3c795d38349841a7130c7a3b21
Ed McClanahan <ed@swiftstack.com>
2019-08-29 11:39:55 -0700

    Add support for Erasure Coded storage policy in SAIO

--- saio/vagrant_provision.sh
+++ saio/vagrant_provision.sh
@@ -192,10 +192,10 @@ mount /mnt/sdb1
 sudo mkdir /mnt/sdb1/1 /mnt/sdb1/2 /mnt/sdb1/3 /mnt/sdb1/4
 chown swift:swift /mnt/sdb1/*
 for x in {1..4}; do ln -s /mnt/sdb1/$x /srv/$x; done
-mkdir -p /srv/1/node/sdb1 /srv/1/node/sdb5 \
-         /srv/2/node/sdb2 /srv/2/node/sdb6 \
-         /srv/3/node/sdb3 /srv/3/node/sdb7 \
-         /srv/4/node/sdb4 /srv/4/node/sdb8 \
+mkdir -p /srv/1/node/sdb1 /srv/1/node/sdb5 /srv/1/node/sdb9 \
+         /srv/2/node/sdb2 /srv/2/node/sdb6 /srv/2/node/sdbA \
+         /srv/3/node/sdb3 /srv/3/node/sdb7 /srv/3/node/sdbB \
+         /srv/4/node/sdb4 /srv/4/node/sdb8 /srv/4/node/sdbC \
          /var/run/swift
 chown -R swift:swift /var/run/swift
 for x in {1..4}; do chown -R swift:swift /srv/$x/; done
