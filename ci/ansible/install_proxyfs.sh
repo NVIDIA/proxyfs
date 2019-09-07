517ea1cadbf76bc3c16c347776ef53bd2f08f106
Gerard Giné <ggine@swiftstack.com>
2019-09-06 18:38:59 -0700

    Multiple golang installs (#370)
    Update ansible/chef files to install multiple golang versions
    Add goswitch script for easy version switching

--- ci/ansible/install_proxyfs.sh
+++ ci/ansible/install_proxyfs.sh
@@ -4,6 +4,7 @@ set -e
 
 ENV_NAME=$1
 SS_PACKAGES=$2
+GOLANG_VERSION=$3
 
 if [ -z "$ENV_NAME" ]; then
   echo "usage: $0 <env-name>"
@@ -14,7 +15,11 @@ if [ -n "$SS_PACKAGES" ] && [ "$ENV_NAME" == "runway" ] && [ "$SS_PACKAGES" == "
     ENV_NAME="runway_ss"
 fi
 
+if [ -z "$GOLANG_VERSION" ]; then
+  GOLANG_VERSION="current"
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ansible-playbook -i "localhost," -c local -e env=$ENV_NAME -e env_arg="$1" -e ss_packages_arg="$2" "$SCRIPT_DIR"/tasks/main.yml
+ansible-playbook -i "localhost," -c local -e env=$ENV_NAME -e env_arg="$1" -e ss_packages_arg="$2" -e golang_version="$GOLANG_VERSION" "$SCRIPT_DIR"/tasks/main.yml
 chef-solo -c "$SCRIPT_DIR"/chef_files/$ENV_NAME.cfg
