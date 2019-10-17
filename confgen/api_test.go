a3251bf908d5018ab0a83380a04b82525608dd23
Bob Schatz <bschatz@swiftstack.com>
2019-10-17 12:28:33 -0700

    Figured out global variables solution

--- confgen/api_test.go
+++ confgen/api_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"testing"
 
 	"text/template"
@@ -33,39 +32,31 @@ type SMBConfig struct {
 	PrivateProxyFSIP  string
 	TCPPort           string
 	FastTCPPort       string
-
-	// NOTE: The rest of this struct creates globals used within
-	// the template.
-	AdCommentStr   string
-	AdIDCommentStr string
 }
 
 // TODO - show basic code for printing smb.conf for my 3 VGs...
 func TestParser(t *testing.T) {
+	// TODO - handle case where no VGs and therefore do not generate any smb.conf files...
 
 	// One entry per VG - each share represents a different volume
 	var mySMB = []SMBConfig{
 		{"workgroup1", "map1", "host1", "proto1", true, false, "some security", "lsa1", "realm1",
-			"ID1", 1, 1, 11, 11, "back1", "", "", "", "", "", ""},
+			"ID1", 1, 1, 11, 11, "back1", "", "", "", ""},
 		{"workgroup2", "map2", "host2", "proto2", false, false, "more security", "lsa2", "realm2",
-			"ID2", 2, 2, 22, 22, "back2", "", "", "", "", "", ""},
+			"ID2", 2, 2, 22, 22, "back2", "", "", "", ""},
 		{"workgroup3", "map3", "host3", "proto3", true, true, "even more security", "lsa3", "realm3",
-			"ID3", 3, 3, 33, 33, "back3", "", "", "", "", "", ""},
+			"ID3", 3, 3, 33, 33, "back3", "", "", "", ""},
 	}
 
-	// pattern is the glob pattern used to find all the template files.
-	pattern := filepath.Join("testData", "smb_conf.tmpl")
-
-	// Here starts the example proper.
-	// Load the drivers.
-	tplate, err := template.ParseGlob(pattern)
+	// Load the template for the global section of smb.conf
+	tplate, err := template.ParseFiles("testData/smb_globals.tmpl")
 	if err != nil {
 		fmt.Printf("Parse of template file returned err: %v\n", err)
 		os.Exit(-1)
 	}
 	fmt.Printf("parsed file - tplate: %v\n", tplate)
 
-	// Execute the template for each SMB configuration
+	// Execute the global template
 	for _, c := range mySMB {
 		err := tplate.Execute(os.Stdout, c)
 		if err != nil {
