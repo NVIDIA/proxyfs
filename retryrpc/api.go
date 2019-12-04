2b52c2756d5108ca36a3111d3528ccf0a5633149
Bob Schatz <bschatz@swiftstack.com>
2019-12-03 16:23:44 -0800

    First pass at retryrpc API and sample usage

---
+++ retryrpc/api.go
@@ -0,0 +1,106 @@
+// Package retryrpc provides a client and server RPC model which survives
+// lost connections on either the client or the server.
+package retryrpc
+
+import (
+	"container/list"
+	"sync"
+	"time"
+)
+
+// Request is the structure sent
+type Request struct {
+	Len       uint64 // Length of this struct including len field
+	RequestID uint64 // Unique ID of this request
+	MountID   uint64 // Mount ID
+	Req       []byte // JSON containing request
+}
+
+// Response is the structure returned
+type Response struct {
+	Len uint64 // Length of this struct including len field
+	Res []byte // JSON response - only valid if err != nil
+	Err error  // Status of request
+}
+
+type completedRequest struct {
+	completedTime time.Time
+	request       *Request
+}
+
+// Server tracks the state of the server
+type Server struct {
+	sync.Mutex
+	completedTTL      time.Duration // How long a completed request stays on queue
+	serviceMap        map[string]*func(request interface{}, response interface{})
+	connections       *list.List                   // List of outstanding connections
+	pendingRequests   map[string]*Request          // Request which have not been completed yet.
+	completedRequests map[string]*completedRequest // Request which have been completed.  Will age out
+	ipaddr            string                       // IP address server listens too
+	port              int                          // Port of server
+}
+
+// NewServer creates the Server object
+func NewServer(ttl time.Duration, ipaddr string, port int) *Server {
+	s := &Server{}
+	s.serviceMap = make(map[string]*func(request interface{}, response interface{}))
+	s.connections = list.New()
+	s.completedTTL = ttl
+	s.ipaddr = ipaddr
+	s.port = port
+	return s
+}
+
+// Register creates the map of server methods
+func (server *Server) Register() (err error) {
+
+	// TODO - build the serviceMap similiar to how net/rpc Register does it...
+	return
+}
+
+// Run server loop, accept connections, read request, run RPC method and
+// return the results.
+func (server *Server) Run() {
+
+}
+
+// Close stops the server
+func (server *Server) Close() {
+	// TODO - blocks until the server is shut down
+
+}
+
+// Client methods
+
+// Client tracking structure
+type Client struct {
+	mountID          uint64
+	currentRequestID uint64
+}
+
+// NewClient returns a Client structure
+func NewClient(mountID uint64) *Client {
+	// TODO - if restart client, Client Request ID will be 0.   How know the server
+	// has removed these client IDs from it's queue?  Race condition...
+	c := &Client{mountID: mountID}
+	return c
+}
+
+// MakeRPC takes the RPC method and arguments and returns a Request struct
+func MakeRPC(method string, params []byte) (request *Request) {
+	// This will:
+	// 1. bump the client request ID
+	// 2. Store the RPC method in the request
+	// 3. Store the parameters in the request
+	// 4. return the request
+	return
+}
+
+// Send the request and block until it has completed
+func (client *Client) Send(request *Request) (response *Response, err error) {
+
+	// TODO - puts request on queue and sends, only when request returns do we take it
+	// off the queue
+	// if connection drops, get new connection and resend the request
+	return
+}
---
+++ retryrpc/api_test.go
@@ -0,0 +1,53 @@
+package retryrpc
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test basic retryrpc primitives
+func TestRetryRPC(t *testing.T) {
+
+	testServer(t)
+}
+
+// Test basic Server creation and deletion
+func testServer(t *testing.T) {
+	assert := assert.New(t)
+	zero := 0
+	assert.Equal(0, zero)
+
+	// Create a new Server.  Completed request will live on
+	// completedRequests for 5 seconds.
+	s := NewServer(5*time.Second, "127.0.0.1", 666)
+	assert.NotNil(s)
+
+	// Register the Server - sets up the methods supported by the
+	// server
+	// TODO - how do this while supporting several different methods???
+	// Must pass interface{} somewhere....
+	err := s.Register()
+	assert.Nil(err)
+
+	// Server starts listening for requests on the ipaddr/port
+	go s.Run()
+
+	// TODO - do some work.....
+
+	// Now - setup a client to send requests to the server
+	c := NewClient(1)
+	assert.Nil(c)
+
+	// Make a new request and send to server
+	// TODO - how create the JSON???
+	request := MakeRPC("Server.RpcPing", params)
+	response, sendErr := c.Send(request)
+	assert.Nil(sendErr)
+	assert.Nil(response.Err)
+
+	// Stop the server before exiting
+	s.Close()
+
+}
