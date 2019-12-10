ae4c55d6b573f72d97645da89d80ebc4cdee34f0
Bob Schatz <bschatz@swiftstack.com>
2019-12-10 13:57:57 -0800

    Cleanup the locking and close holes

--- retryrpc/client.go
+++ retryrpc/client.go
@@ -35,11 +35,15 @@ import (
 func (client *Client) send(method string, rpcRequest interface{}) (reply *Reply, err error) {
 	var crID uint64
 
+	// TODO - Serialize writes on the socket to the server
+
 	// Put request data into structure to be be marshaled into JSON
 	jreq := jsonRequest{Method: method}
 	jreq.Params[0] = rpcRequest
 	jreq.MyUniqueID = client.myUniqueID
 
+	// Grab ours under the lock since it could change once
+	// we drop the lock
 	client.Lock()
 	client.currentRequestID++
 	crID = client.currentRequestID
@@ -50,11 +54,6 @@ func (client *Client) send(method string, rpcRequest interface{}) (reply *Reply,
 	req.JReq, err = json.Marshal(jreq)
 	req.Len = int64(len(req.JReq))
 
-	fmt.Printf("req.Len is : %v\n", req.Len)
-
-	// TODO TODO - how serializes send() requests so no intermixed
-	// writes...
-
 	// Keep track of requests we are sending so we can resend them later as
 	// needed.
 	client.Lock()
@@ -62,8 +61,11 @@ func (client *Client) send(method string, rpcRequest interface{}) (reply *Reply,
 	client.Unlock()
 
 	// Now send the request to the server and retry operation if it fails
+	// We need to grab the mutex here to prevent intermixed I/Os
+	client.Lock()
 
-	// Send length - how do in one?
+	// Send length - how do whole request in one I/O?
+	//
 	// This is how you hton() in Golang
 	err = binary.Write(client.tcpConn, binary.BigEndian, req.Len)
 	if err != nil {
@@ -75,6 +77,12 @@ func (client *Client) send(method string, rpcRequest interface{}) (reply *Reply,
 	bytesWritten, writeErr := client.tcpConn.Write(req.JReq)
 	fmt.Printf("CLIENT: Wrote RPC REQEUST with bytesWritten: %v writeErr: %v\n", bytesWritten, writeErr)
 
+	// Drop the lock once we wrote the request.
+	client.Unlock()
+
+	// TODO - move this into a goroutine.... use channel to pick up our response
+	// since we could see anyones.....
+
 	// Wait reply
 	buf, getErr := getIO(client.tcpConn, "CLIENT")
 	if getErr != nil {
@@ -82,9 +90,6 @@ func (client *Client) send(method string, rpcRequest interface{}) (reply *Reply,
 		err = getErr
 		return
 	}
-	fmt.Printf("CLIENT: Saw response: %+v\n", buf)
-
-	// TODO - Unmarshal this back to rpcReply and return it...
 
 	// Unmarshal back once to get the header fields
 	jReply := jsonReply{}
@@ -94,13 +99,19 @@ func (client *Client) send(method string, rpcRequest interface{}) (reply *Reply,
 		return
 	}
 
-	// Now get the result field
+	// Now unmarshal again to get the result field
 	pingReply := pingJSONReply{}
 	err = json.Unmarshal(buf, &pingReply)
 	if err != nil {
 		fmt.Printf("CLIENT: Unmarshal of buf failed with err: %v\n", err)
 		return
 	}
+
+	// Remove from client.outstandingRequest
+	client.Lock()
+	delete(client.outstandingRequest, crID)
+	client.Unlock()
+
 	fmt.Printf("CLIENT: jReply: %+v\n", jReply)
 	fmt.Printf("CLIENT: pingReply.Result: %+v\n", pingReply.Result)
 
