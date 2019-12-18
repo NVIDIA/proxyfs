01ce89d613faa010519dfa1df7187a8729e98374
Bob Schatz <bschatz@swiftstack.com>
2019-12-18 14:59:14 -0800

    Add emulation of multiple pfsagents and multiple threads

--- pfs-retryrpc/main.go
+++ pfs-retryrpc/main.go
@@ -5,12 +5,55 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
 
 	"github.com/swiftstack/ProxyFS/conf"
 	"github.com/swiftstack/ProxyFS/jrpcfs"
 	"github.com/swiftstack/ProxyFS/logger"
 	"github.com/swiftstack/ProxyFS/retryrpc"
 )
+
+func sendIt(client *retryrpc.Client, i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Send a ping RPC and print the results
+	msg := fmt.Sprintf("Ping Me - %v\n", i)
+	pingRequest := &jrpcfs.PingReq{Message: msg}
+	pingReply := &jrpcfs.PingReply{}
+	err := client.Send("RpcPing", pingRequest, pingReply)
+	if err != nil {
+		fmt.Printf("client.Send(RpcPing) failed with err: %v\n", err)
+		os.Exit(-1)
+	}
+	fmt.Printf("client.Send(RpcPing) returned reply: %+v\n", pingReply)
+}
+
+// Represents a pfsagent - sepearate client
+func pfsagent(ipAddr string, retryRPCPortString string, i int, agentWG *sync.WaitGroup) {
+	defer agentWG.Done()
+
+	// 1. setup client and connect to proxyfsd
+	// 2. loop doing RPCs in parallel
+	clientID := fmt.Sprintf("client - %v", i)
+	client := retryrpc.NewClient(clientID)
+
+	// Have client connect to server
+	port, _ := strconv.Atoi(retryRPCPortString)
+	client.Dial(ipAddr, port)
+
+	var wg sync.WaitGroup
+
+	var z int
+	for i := 0; i < 1000; i++ {
+		z = (z + i) * 10
+
+		wg.Add(1)
+		go sendIt(client, z, &wg)
+	}
+	wg.Wait()
+
+	client.Close()
+}
 
 func main() {
 	var (
@@ -66,21 +109,15 @@ func main() {
 		return
 	}
 
-	// 1. setup client and connect to proxyfsd
-	// 2. do one ping request and print results
-	client := retryrpc.NewClient("client 1")
+	var wg sync.WaitGroup
 
-	// Have client connect to server
-	port, _ := strconv.Atoi(retryRPCPortString)
-	client.Dial(ipAddr, port)
+	// Start parallel pfsagents each doing parallel sends
+	var z int
+	for i := 0; i < 100; i++ {
+		z = (z + i) * 10
 
-	// Send a ping RPC and print the results
-	pingRequest := &jrpcfs.PingReq{Message: "Ping Me!"}
-	pingReply := &jrpcfs.PingReply{}
-	err = client.Send("RpcPing", pingRequest, pingReply)
-	if err != nil {
-		fmt.Printf("client.Send(RpcPing) failed with err: %v\n", err)
-		os.Exit(-1)
+		wg.Add(1)
+		go pfsagent(ipAddr, retryRPCPortString, z, &wg)
 	}
-	fmt.Printf("client.Send(RpcPing) returned reply: %+v\n", pingReply)
+	wg.Wait()
 }
