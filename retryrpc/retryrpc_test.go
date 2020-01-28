4f8d7a4fb4f5fbbc67b7976db1c49f997d0d382a
Bob Schatz <bschatz@swiftstack.com>
2020-01-28 11:41:33 -0800

    Fix bug in calculating highest consecutive number

--- retryrpc/retryrpc_test.go
+++ retryrpc/retryrpc_test.go
@@ -120,49 +120,36 @@ type RequestID uint64
 // If !a.Less(b) && !b.Less(a), we treat this to mean a == b (i.e. we can only
 // hold one of either a or b in the tree).
 func (a RequestID) Less(b btree.Item) bool {
-	//fmt.Printf("\tLess() - a: %v b.(RequestID): %v return: %v\n", a, b.(RequestID), a < b.(RequestID))
 	return a < b.(RequestID)
 }
 
 /* TODO - test case
-1. simulate responses coming in out of order - 5, 10, 20, 1, 3, 2, etc
-2. find "highest consecutive RequestID - (no gaps in numbers)"
-3. simulate gaps being filled
-4. simulate deleting and asserting that have correct numbers
 5. evaluate storing outstandingRequest as btree or having only numbers in btree
+	== probably only want ints in btree and not complete data structure in
+	case one RPC hangs....
 */
 
 func setHighestConsecutive(highestConsecutiveNum *RequestID, tr *btree.BTree) {
 	fmt.Printf("setHighestConsecutive()\n")
 	tr.Ascend(func(a btree.Item) bool {
 		r := a.(RequestID)
-		/*
-			c := *highestConsecutiveNum
-		*/
-		fmt.Printf("r is: %v\n", r)
+		c := *highestConsecutiveNum
 
-		/*
-			// If this item is a consecutive number then keep going.
-			// Otherwise stop the Ascend now
-			c++
-			if r == *highestConsecutiveNum {
-				*highestConsecutiveNum = r
-				fmt.Printf("highestConsecutiveNum now: %v\n", highestConsecutiveNum)
-				return true
-			}
-		*/
-		if r == tr.Max() {
+		// If this item is a consecutive number then keep going.
+		// Otherwise stop the Ascend now
+		c++
+		if r == c {
+			*highestConsecutiveNum = r
+		} else {
+			// If not consective just return
 			return false
 		}
 		return true
 	})
-	return
 }
 
 func wrapper(tr *btree.BTree, r RequestID) {
-	//fmt.Printf("BEFORE - ReplaceOrInsert(): %v - Min(): %v\n", r, tr.Min())
 	tr.ReplaceOrInsert(r)
-	fmt.Printf("AFTER - ReplaceOrInsert(): %v - Min(): %v\n", r, tr.Min())
 }
 
 func testBtree(t *testing.T) {
@@ -177,8 +164,6 @@ func testBtree(t *testing.T) {
 	wrapper(tr, RequestID(5))
 	wrapper(tr, RequestID(11))
 
-	fmt.Printf("minimum value: %v\n", tr.Min())
-
 	setHighestConsecutive(&highestConsecutiveNum, tr)
 	assert.Equal(RequestID(0), highestConsecutiveNum)
 
@@ -187,27 +172,29 @@ func testBtree(t *testing.T) {
 	wrapper(tr, RequestID(3))
 	wrapper(tr, RequestID(2))
 	wrapper(tr, RequestID(1))
-	fmt.Printf("new minimum value: %v len: %v\n", tr.Min(), tr.Len())
 	assert.Equal(int(7), tr.Len())
 
 	setHighestConsecutive(&highestConsecutiveNum, tr)
 	assert.Equal(RequestID(5), highestConsecutiveNum)
 
-	/*
-		for i := RequestID(0); i < 10; i++ {
-			tr.ReplaceOrInsert(i)
-		}
-		fmt.Println("len:       ", tr.Len())
-		fmt.Println("get3:      ", tr.Get(RequestID(3)))
-		fmt.Println("get100:    ", tr.Get(RequestID(100)))
-		fmt.Println("del4:      ", tr.Delete(RequestID(4)))
-		fmt.Println("del100:    ", tr.Delete(RequestID(100)))
-		fmt.Println("replace5:  ", tr.ReplaceOrInsert(RequestID(5)))
-		fmt.Println("replace100:", tr.ReplaceOrInsert(RequestID(100)))
-		fmt.Println("min:       ", tr.Min())
-		fmt.Println("delmin:    ", tr.DeleteMin())
-		fmt.Println("max:       ", tr.Max())
-		fmt.Println("delmax:    ", tr.DeleteMax())
-		fmt.Println("len:       ", tr.Len())
-	*/
+	// Now fillin next set of gaps and getting highestConsecutiveNum
+	wrapper(tr, RequestID(6))
+	wrapper(tr, RequestID(7))
+	wrapper(tr, RequestID(8))
+	wrapper(tr, RequestID(9))
+	assert.Equal(int(11), tr.Len())
+
+	setHighestConsecutive(&highestConsecutiveNum, tr)
+	assert.Equal(RequestID(11), highestConsecutiveNum)
+
+	// Now delete client list and btree entry...
+	// TODO - setHighestConsecutive() should probably
+	// delete earlier btree entries since no longer
+	// needed..
+	for i := 1; i < 12; i++ {
+		tr.Delete(RequestID(i))
+	}
+	assert.Equal(int(0), tr.Len())
+	setHighestConsecutive(&highestConsecutiveNum, tr)
+	assert.Equal(RequestID(11), highestConsecutiveNum)
 }
