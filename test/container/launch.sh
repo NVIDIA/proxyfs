86c4341ad31d52d859f13819ae137befa58ff8c2
Gerard Giné <ggine@swiftstack.com>
2020-02-19 13:19:51 -0800

    Fix coveralls reports
    We weren't properly sending the branch name, so all the reports were
    shown as detached from HEAD.
    
    Also:
     - Add some binary dirs to coverage
     - Go get go-acc on pre-generate make target

--- test/container/launch.sh
+++ test/container/launch.sh
@@ -40,5 +40,6 @@ cd $GOPATH/src/github.com/swiftstack/ProxyFS
 make ci
 # $COVERALLS_TOKEN must be configured in TravisCI
 if [ -n "$COVERALLS_TOKEN" ]; then
-    goveralls -coverprofile coverage.coverprofile -service travis-ci -repotoken $COVERALLS_TOKEN
+    GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    GIT_BRANCH=$GIT_BRANCH goveralls -coverprofile coverage.coverprofile -service travis-ci -repotoken $COVERALLS_TOKEN
 fi
