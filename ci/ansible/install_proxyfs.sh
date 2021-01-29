b8a45311937ca28cf797b549910499f355811416
Gerard Giné <ggine@nvidia.com>
2021-01-29 15:41:07 -0800

    Stop installing smb nfs runway (#577)
    This means we can stop using "SwiftStack packages", so removing all the
    references to that from code as well.

--- ci/ansible/install_proxyfs.sh
+++ ci/ansible/install_proxyfs.sh
@@ -6,16 +6,11 @@
 set -e
 
 ENV_NAME=$1
-SS_PACKAGES=$2
-GOLANG_VERSION=$3
+GOLANG_VERSION=$2
 
 if [ -z "$ENV_NAME" ]; then
   echo "usage: $0 <env-name>"
   exit 1
-fi
-
-if [ -n "$SS_PACKAGES" ] && [ "$ENV_NAME" == "runway" ] && [ "$SS_PACKAGES" == "swiftstack" ]; then
-    ENV_NAME="runway_ss"
 fi
 
 if [ -z "$GOLANG_VERSION" ]; then
@@ -24,5 +19,5 @@ fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ansible-playbook -i "localhost," -c local -e env=$ENV_NAME -e env_arg="$1" -e ss_packages_arg="$2" -e golang_version="$GOLANG_VERSION" "$SCRIPT_DIR"/tasks/main.yml
+ansible-playbook -i "localhost," -c local -e env=$ENV_NAME -e env_arg="$1" -e golang_version="$GOLANG_VERSION" "$SCRIPT_DIR"/tasks/main.yml
 chef-solo -c "$SCRIPT_DIR"/chef_files/$ENV_NAME.cfg
