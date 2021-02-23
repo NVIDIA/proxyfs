d3435d22421aa1312c3e80ac5aee516dc9599502
Ed McClanahan <emcclanahan@nvidia.com>
2021-02-23 15:54:38 -0800

    Added templates for each of the three RPC/API servers in imgr

---
+++ imgr/imgrpkg/json-rpc.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2015-2021, NVIDIA CORPORATION.
+// SPDX-License-Identifier: Apache-2.0
+
+package imgrpkg
+
+func startJSONRPCServer() (err error) {
+	return nil // TODO
+}
+
+func stopJSONRPCServer() (err error) {
+	return nil // TODO
+}
