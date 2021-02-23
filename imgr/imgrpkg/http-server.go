69fe8e3fb0ed2346d88aae45b95af42c7edd923c
Ed McClanahan <emcclanahan@nvidia.com>
2021-02-23 15:54:38 -0800

    Added templates for each of the three RPC/API servers in imgr

---
+++ imgr/imgrpkg/http-server.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2015-2021, NVIDIA CORPORATION.
+// SPDX-License-Identifier: Apache-2.0
+
+package imgrpkg
+
+func startHTTPRPCServer() (err error) {
+	return nil // TODO
+}
+
+func stopHTTPRPCServer() (err error) {
+	return nil // TODO
+}
---
+++ imgr/imgrpkg/retry-rpc.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2015-2021, NVIDIA CORPORATION.
+// SPDX-License-Identifier: Apache-2.0
+
+package imgrpkg
+
+func startRetryRPCServer() (err error) {
+	return nil // TODO
+}
+
+func stopRetryRPCServer() (err error) {
+	return nil // TODO
+}
