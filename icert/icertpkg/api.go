638cf16887a869516e2e7ddd5997b0f2b35483e5
Ed McClanahan <emcclanahan@nvidia.com>
2021-03-25 09:27:55 -0700

    Updated icert/icertpkg to work with both []byte and file path (string) certs and keys

--- icert/icertpkg/api.go
+++ icert/icertpkg/api.go
@@ -38,26 +38,32 @@ const (
 
 // GenCACert is called to generate a Certificate Authority using the requested
 // generateKeyAlgorithm for the specified subject who's validity last for the
-// desired ttl starting from time.Now(). The resultant PEM-encoded CA Certificate
-// is written to certFile. The PEM-encoded private key for the CA Certificate is
-// written to keyFile. If certFile and keyFile are the same, both the CA Certificate
-// and its private key will be written to the common file.
+// desired ttl starting from time.Now(). The resultant PEM-encoded certPEMBlock
+// and keyPEMBlock are returned. Optionally, certPEMBlock will be written to
+// certFile (if not "") and/or keyPEMBlock will be written to keyFile (if not "").
+// If certFile and keyFile are the same, both the CA Certificate and its Private
+// Key will be written to the common file.
 //
-func GenCACert(generateKeyAlgorithm string, subject pkix.Name, ttl time.Duration, certFile string, keyFile string) (err error) {
-	return genCACert(generateKeyAlgorithm, subject, ttl, certFile, keyFile)
+func GenCACert(generateKeyAlgorithm string, subject pkix.Name, ttl time.Duration, certFile string, keyFile string) (certPEMBlock []byte, keyPEMBlock []byte, err error) {
+	certPEMBlock, keyPEMBlock, err = genCACert(generateKeyAlgorithm, subject, ttl, certFile, keyFile)
+	return
 }
 
 // GenEndpointCert is called to generate a Certificate using the requested
 // generateKeyAlgorithm for the specified subject who's validity lasts for
 // the desired ttl starting from time.Now(). The endpoints for which the
 // Certificate will apply will be the specified dnsNames and/or ipAddresses.
-// The Certificate will be signed by the CA Certificate specified via caCertFile
-// and caKeyFile. The caCertFile and caKeyFile values may be identical. The
-// resultant PEM-encoded Certificate is written to certFile. The PEM-encoded
-// private key for the Certificate is written to keyFile. If certFile and
-// keyFile are the same, both the Certificate and its private key will be
-// written to the common file.
+// The Certificate will be signed by the CA Certificate specified via
+// caCert and caKey. Note that caCert and caKey may either be PEMBlock
+// byte slices ([]byte) or file path strings. If caCert and caKey are both
+// strings, they may refer to a common file. The resulted PEM-encoded
+// endpoingCertPEMBlock and endpointKeyPEMBlock are returned. Optionally,
+// endpoingCertPEMBlock will be written to endpointCertFile (if not "")
+// and/or endpointKeyPEMBlock will be written to endpointKeyFile (if not "").
+// If endpointCertFile and endpointKeyFile are the same, both the Endpoint
+// Certificate and its Private Key will be written to the common file.
 //
-func GenEndpointCert(generateKeyAlgorithm string, subject pkix.Name, dnsNames []string, ipAddresses []net.IP, ttl time.Duration, caCertFile string, caKeyFile string, endpointCertFile string, endpointKeyFile string) (err error) {
-	return genEndpointCert(generateKeyAlgorithm, subject, dnsNames, ipAddresses, ttl, caCertFile, caKeyFile, endpointCertFile, endpointKeyFile)
+func GenEndpointCert(generateKeyAlgorithm string, subject pkix.Name, dnsNames []string, ipAddresses []net.IP, ttl time.Duration, caCert interface{}, caKey interface{}, endpointCertFile string, endpointKeyFile string) (endpointCertPEMBlock []byte, endpointKeyPEMBlock []byte, err error) {
+	endpointCertPEMBlock, endpointKeyPEMBlock, err = genEndpointCert(generateKeyAlgorithm, subject, dnsNames, ipAddresses, ttl, caCert, caKey, endpointCertFile, endpointKeyFile)
+	return
 }
