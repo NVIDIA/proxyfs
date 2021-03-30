8c01b2c31271e72522d29bfa9d0af01f15ce1b27
Ed McClanahan <emcclanahan@nvidia.com>
2021-03-30 14:32:32 -0700

    Added shortcut to retryrpc benchmarks (both TCP and TLS) to jrpcfs RpcLease tests

--- jrpcfs/lease_test.go
+++ jrpcfs/lease_test.go
@@ -4,8 +4,13 @@
 package jrpcfs
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net"
 	"os"
 	"runtime"
 	"strings"
@@ -17,16 +22,19 @@ import (
 )
 
 const (
-	testRpcLeaseDelayAfterSendingRequest         = 10 * time.Millisecond
-	testRpcLeaseDelayBeforeSendingRequest        = 10 * time.Millisecond
-	testRpcLeaseRetryRPCDeadlineIO               = "60s"
-	testRpcLeaseRetryRPCKeepAlivePeriod          = "60s"
-	testRpcLeaseMultiFirstInodeNumber     int64  = 1
-	testRpcLeaseMultiNumInstances         int    = 5
-	testRpcLeaseSingleInodeNumber         int64  = 1
-	testRpcLeaseSingleNumInstances        int    = 101 // Must be >= 4
-	testRpcLeaseTimeFormat                       = "15:04:05.000"
-	testRpcLeaseLocalClientID             uint64 = 0
+	testRpcLeaseDelayAfterSendingRequest            = 10 * time.Millisecond
+	testRpcLeaseDelayBeforeSendingRequest           = 10 * time.Millisecond
+	testRpcLeaseRetryRPCDeadlineIO                  = "60s"
+	testRpcLeaseRetryRPCKeepAlivePeriod             = "60s"
+	testRpcLeaseMultiFirstInodeNumber        int64  = 1
+	testRpcLeaseMultiNumInstances            int    = 5
+	testRpcLeaseSingleInodeNumber            int64  = 1
+	testRpcLeaseSingleNumInstances           int    = 101 // Must be >= 4
+	testRpcLeaseTimeFormat                          = "15:04:05.000"
+	testRpcLeaseLocalClientID                uint64 = 0
+	testRpcLeaseShortcutIPAddrPort                  = "127.0.0.1:24680"
+	testRpcLeaseShortcutClientConnMaxRetries        = 10
+	testRpcLeaseShortcutClientConnRetryDelay        = "100ms"
 )
 
 var (
@@ -46,6 +54,452 @@ type testRpcLeaseClientStruct struct {
 	t                *testing.T
 }
 
+type benchmarkRpcShortcutRequestMethodOnlyStruct struct { // preceeded by a uint32 encoding length in LittleEndian form
+	Method string
+}
+
+type benchmarkRpcShortcutLeaseRequestStruct struct { // preceeded by a uint32 encoding length in LittleEndian form
+	Method  string
+	Request *LeaseRequest
+}
+
+type benchmarkRpcShortcutMountByAccountNameRequestStruct struct { // preceeded by a uint32 encoding length in LittleEndian form
+	Method  string
+	Request *MountByAccountNameRequest
+}
+
+type benchmarkRpcShortcutUnmountRequestStruct struct { // preceeded by a uint32 encoding length in LittleEndian form
+	Method  string
+	Request *UnmountRequest
+}
+
+type benchmarkRpcShortcutLeaseReplyStruct struct { // preceeded by a uint32 encoding length in LittleEndian form
+	Err   error
+	Reply *LeaseReply
+}
+
+type benchmarkRpcShortcutMountByAccountNameReplyStruct struct { // preceeded by a uint32 encoding length in LittleEndian form
+	Err   error
+	Reply *MountByAccountNameReply
+}
+
+type benchmarkRpcShortcutUnmountReplyStruct struct { // preceeded by a uint32 encoding length in LittleEndian form
+	Err   error
+	Reply *Reply
+}
+
+func BenchmarkRpcLeaseShortcutTCP(b *testing.B) {
+	benchmarkRpcLeaseShortcut(b, false)
+}
+
+func BenchmarkRpcLeaseShortcutTLS(b *testing.B) {
+	benchmarkRpcLeaseShortcut(b, true)
+}
+
+func benchmarkRpcLeaseShortcut(b *testing.B, useTLS bool) {
+	var (
+		benchmarkIteration               int
+		doneWG                           sync.WaitGroup
+		err                              error
+		leaseReply                       *LeaseReply
+		leaseReplyWrapped                *benchmarkRpcShortcutLeaseReplyStruct
+		leaseRequest                     *LeaseRequest
+		leaseRequestWrapped              *benchmarkRpcShortcutLeaseRequestStruct
+		mountByAccountNameReply          *MountByAccountNameReply
+		mountByAccountNameReplyWrapped   *benchmarkRpcShortcutMountByAccountNameReplyStruct
+		mountByAccountNameRequest        *MountByAccountNameRequest
+		mountByAccountNameRequestWrapped *benchmarkRpcShortcutMountByAccountNameRequestStruct
+		netConn                          net.Conn
+		netConnRetries                   int
+		netConnRetryDelay                time.Duration
+		ok                               bool
+		rootCACertPool                   *x509.CertPool
+		tlsConfig                        *tls.Config
+		tlsConn                          *tls.Conn
+		unmountReply                     *Reply
+		unmountReplyWrapped              *benchmarkRpcShortcutUnmountReplyStruct
+		unmountRequest                   *UnmountRequest
+		unmountRequestWrapped            *benchmarkRpcShortcutUnmountRequestStruct
+	)
+
+	doneWG.Add(1)
+	go benchmarkRpcLeaseShortcutTCPServer(useTLS, &doneWG)
+
+	netConnRetries = 0
+
+	netConnRetryDelay, err = time.ParseDuration(testRpcLeaseShortcutClientConnRetryDelay)
+	if nil != err {
+		b.Fatalf("time.ParseDuration(testRpcLeaseShortcutClientConnRetryDelay=\"%s\") failed: %v", testRpcLeaseShortcutClientConnRetryDelay, err)
+	}
+
+	if useTLS {
+		rootCACertPool = x509.NewCertPool()
+		ok = rootCACertPool.AppendCertsFromPEM(testTLSCerts.caCertPEMBlock)
+		if !ok {
+			b.Fatalf("rootCACertPool.AppendCertsFromPEM(testTLSCerts.caCertPEMBlock) returned !ok")
+		}
+		tlsConfig = &tls.Config{
+			RootCAs: rootCACertPool,
+		}
+	}
+
+	for {
+		if useTLS {
+			tlsConn, err = tls.Dial("tcp", testRpcLeaseShortcutIPAddrPort, tlsConfig)
+			if nil == err {
+				netConn = tlsConn
+				break
+			}
+		} else {
+			netConn, err = net.Dial("tcp", testRpcLeaseShortcutIPAddrPort)
+			if nil == err {
+				break
+			}
+		}
+
+		netConnRetries++
+
+		if netConnRetries > testRpcLeaseShortcutClientConnMaxRetries {
+			b.Fatalf("netConnRetries exceeded testRpcLeaseShortcutClientConnMaxRetries (%v)", testRpcLeaseShortcutClientConnMaxRetries)
+		}
+
+		time.Sleep(netConnRetryDelay)
+	}
+
+	mountByAccountNameRequest = &MountByAccountNameRequest{
+		AccountName: testAccountName,
+		AuthToken:   "",
+	}
+	mountByAccountNameReply = &MountByAccountNameReply{}
+
+	mountByAccountNameRequestWrapped = &benchmarkRpcShortcutMountByAccountNameRequestStruct{Method: "RpcMountByAccountName", Request: mountByAccountNameRequest}
+	mountByAccountNameReplyWrapped = &benchmarkRpcShortcutMountByAccountNameReplyStruct{Reply: mountByAccountNameReply}
+
+	benchmarkRpcLeaseShortcutDoRequest(b, netConn, mountByAccountNameRequestWrapped, mountByAccountNameReplyWrapped)
+	if nil != mountByAccountNameReplyWrapped.Err {
+		b.Fatalf("benchmarkRpcLeaseShortcutDoRequest(mountByAccountNameRequestWrapped, mountByAccountNameReplyWrapped) failed: %v", mountByAccountNameReplyWrapped.Err)
+	}
+
+	b.ResetTimer()
+
+	for benchmarkIteration = 0; benchmarkIteration < b.N; benchmarkIteration++ {
+		leaseRequest = &LeaseRequest{
+			InodeHandle: InodeHandle{
+				MountID:     mountByAccountNameReply.MountID,
+				InodeNumber: testRpcLeaseSingleInodeNumber,
+			},
+			LeaseRequestType: LeaseRequestTypeExclusive,
+		}
+		leaseReply = &LeaseReply{}
+
+		leaseRequestWrapped = &benchmarkRpcShortcutLeaseRequestStruct{Method: "RpcLease", Request: leaseRequest}
+		leaseReplyWrapped = &benchmarkRpcShortcutLeaseReplyStruct{Reply: leaseReply}
+
+		benchmarkRpcLeaseShortcutDoRequest(b, netConn, leaseRequestWrapped, leaseReplyWrapped)
+		if nil != leaseReplyWrapped.Err {
+			b.Fatalf("benchmarkRpcLeaseShortcutDoRequest(leaseRequestWrapped, leaseReplyWrapped) failed: %v", leaseReplyWrapped.Err)
+		}
+
+		if LeaseReplyTypeExclusive != leaseReplyWrapped.Reply.LeaseReplyType {
+			b.Fatalf("RpcLease() returned LeaseReplyType %v... expected LeaseRequestTypeExclusive", leaseReplyWrapped.Reply.LeaseReplyType)
+		}
+
+		leaseRequest = &LeaseRequest{
+			InodeHandle: InodeHandle{
+				MountID:     mountByAccountNameReply.MountID,
+				InodeNumber: testRpcLeaseSingleInodeNumber,
+			},
+			LeaseRequestType: LeaseRequestTypeRelease,
+		}
+		leaseReply = &LeaseReply{}
+
+		leaseRequestWrapped = &benchmarkRpcShortcutLeaseRequestStruct{Method: "RpcLease", Request: leaseRequest}
+		leaseReplyWrapped = &benchmarkRpcShortcutLeaseReplyStruct{Reply: leaseReply}
+
+		benchmarkRpcLeaseShortcutDoRequest(b, netConn, leaseRequestWrapped, leaseReplyWrapped)
+		if nil != leaseReplyWrapped.Err {
+			b.Fatalf("benchmarkRpcLeaseShortcutDoRequest(leaseRequestWrapped, leaseReplyWrapped) failed: %v", leaseReplyWrapped.Err)
+		}
+
+		if LeaseReplyTypeReleased != leaseReplyWrapped.Reply.LeaseReplyType {
+			b.Fatalf("RpcLease() returned LeaseReplyType %v... expected LeaseReplyTypeReleased", leaseReplyWrapped.Reply.LeaseReplyType)
+		}
+	}
+
+	b.StopTimer()
+
+	unmountRequest = &UnmountRequest{
+		MountID: mountByAccountNameReply.MountID,
+	}
+	unmountReply = &Reply{}
+
+	unmountRequestWrapped = &benchmarkRpcShortcutUnmountRequestStruct{Method: "RpcUnmount", Request: unmountRequest}
+	unmountReplyWrapped = &benchmarkRpcShortcutUnmountReplyStruct{Reply: unmountReply}
+
+	benchmarkRpcLeaseShortcutDoRequest(b, netConn, unmountRequestWrapped, unmountReplyWrapped)
+	if nil != unmountReplyWrapped.Err {
+		b.Fatalf("benchmarkRpcLeaseShortcutDoRequest(unmountRequestWrapped, unmountReplyWrapped) failed: %v", unmountReplyWrapped.Err)
+	}
+
+	err = netConn.Close()
+	if nil != err {
+		b.Fatalf("netConn.Close() failed: %v", err)
+	}
+
+	doneWG.Wait()
+}
+
+func benchmarkRpcLeaseShortcutTCPServer(useTLS bool, doneWG *sync.WaitGroup) {
+	var (
+		benchmarkRpcShortcutRequestMethodOnly *benchmarkRpcShortcutRequestMethodOnlyStruct
+		err                                   error
+		jserver                               *Server
+		leaseReply                            *LeaseReply
+		leaseReplyWrapped                     *benchmarkRpcShortcutLeaseReplyStruct
+		leaseRequest                          *LeaseRequest
+		leaseRequestWrapped                   *benchmarkRpcShortcutLeaseRequestStruct
+		mountByAccountNameReply               *MountByAccountNameReply
+		mountByAccountNameReplyWrapped        *benchmarkRpcShortcutMountByAccountNameReplyStruct
+		mountByAccountNameRequest             *MountByAccountNameRequest
+		mountByAccountNameRequestWrapped      *benchmarkRpcShortcutMountByAccountNameRequestStruct
+		n                                     int
+		netConn                               net.Conn
+		netListener                           net.Listener
+		replyBuf                              []byte
+		replyLen                              uint32
+		replyLenBuf                           []byte
+		requestBuf                            []byte
+		requestLen                            uint32
+		requestLenBuf                         []byte
+		tlsConfig                             *tls.Config
+		unmountReply                          *Reply
+		unmountReplyWrapped                   *benchmarkRpcShortcutUnmountReplyStruct
+		unmountRequest                        *UnmountRequest
+		unmountRequestWrapped                 *benchmarkRpcShortcutUnmountRequestStruct
+	)
+
+	jserver = NewServer()
+
+	netListener, err = net.Listen("tcp", testRpcLeaseShortcutIPAddrPort)
+	if nil != err {
+		panic(fmt.Errorf("net.Listen(\"tcp\", testRpcLeaseShortcutIPAddrPort) failed: %v", err))
+	}
+
+	if useTLS {
+		tlsConfig = &tls.Config{
+			Certificates: []tls.Certificate{testTLSCerts.endpointTLSCert},
+		}
+
+		netListener = tls.NewListener(netListener, tlsConfig)
+	}
+
+	netConn, err = netListener.Accept()
+	if nil != err {
+		panic(fmt.Errorf("netListener.Accept() failed: %v", err))
+	}
+
+	for {
+		requestLenBuf = make([]byte, 4)
+
+		n, err = netConn.Read(requestLenBuf)
+		if nil != err {
+			if io.EOF != err {
+				panic(fmt.Errorf("netConn.Read(requestLenBuf) failed: %v", err))
+			}
+
+			err = netConn.Close()
+			if nil != err {
+				panic(fmt.Errorf("netConn.Close() failed: %v", err))
+			}
+
+			err = netListener.Close()
+			if nil != err {
+				panic(fmt.Errorf("netListener.Close() failed: %v", err))
+			}
+
+			doneWG.Done()
+
+			return
+		}
+		if n != 4 {
+			panic(fmt.Errorf("netConn.Read(requestLenBuf) returned n == %v (expected 4)", n))
+		}
+
+		requestLen = binary.LittleEndian.Uint32(requestLenBuf)
+		requestBuf = make([]byte, requestLen)
+
+		n, err = netConn.Read(requestBuf)
+		if nil != err {
+			panic(fmt.Errorf("netConn.Read(requestBuf) failed: %v", err))
+		}
+		if n != int(requestLen) {
+			panic(fmt.Errorf("netConn.Read(requestBuf) returned n == %v (expected %v)", n, requestLen))
+		}
+
+		benchmarkRpcShortcutRequestMethodOnly = &benchmarkRpcShortcutRequestMethodOnlyStruct{}
+
+		err = json.Unmarshal(requestBuf, benchmarkRpcShortcutRequestMethodOnly)
+		if nil != err {
+			panic(fmt.Errorf("json.Unmarshal(requestBuf, benchmarkRpcShortcutRequestMethodOnly) failed: %v", err))
+		}
+
+		switch benchmarkRpcShortcutRequestMethodOnly.Method {
+		case "RpcMountByAccountName":
+			mountByAccountNameRequestWrapped = &benchmarkRpcShortcutMountByAccountNameRequestStruct{}
+
+			err = json.Unmarshal(requestBuf, mountByAccountNameRequestWrapped)
+			if nil != err {
+				panic(fmt.Errorf("json.Unmarshal(requestBuf, mountByAccountNameRequestWrapped) failed: %v", err))
+			}
+
+			mountByAccountNameRequest = mountByAccountNameRequestWrapped.Request
+			mountByAccountNameReply = &MountByAccountNameReply{}
+
+			err = jserver.RpcMountByAccountName(testRpcLeaseLocalClientID, mountByAccountNameRequest, mountByAccountNameReply)
+
+			mountByAccountNameReplyWrapped = &benchmarkRpcShortcutMountByAccountNameReplyStruct{
+				Err:   err,
+				Reply: mountByAccountNameReply,
+			}
+
+			replyBuf, err = json.Marshal(mountByAccountNameReplyWrapped)
+			if nil != err {
+				panic(fmt.Errorf("json.Marshal(mountByAccountNameReplyWrapped) failed"))
+			}
+		case "RpcLease":
+			leaseRequestWrapped = &benchmarkRpcShortcutLeaseRequestStruct{}
+
+			err = json.Unmarshal(requestBuf, leaseRequestWrapped)
+			if nil != err {
+				panic(fmt.Errorf("json.Unmarshal(requestBuf, leaseRequestWrapped) failed: %v", err))
+			}
+
+			leaseRequest = leaseRequestWrapped.Request
+			leaseReply = &LeaseReply{}
+
+			err = jserver.RpcLease(leaseRequest, leaseReply)
+
+			leaseReplyWrapped = &benchmarkRpcShortcutLeaseReplyStruct{
+				Err:   err,
+				Reply: leaseReply,
+			}
+
+			replyBuf, err = json.Marshal(leaseReplyWrapped)
+			if nil != err {
+				panic(fmt.Errorf("json.Marshal(leaseReplyWrapped) failed"))
+			}
+		case "RpcUnmount":
+			unmountRequestWrapped = &benchmarkRpcShortcutUnmountRequestStruct{}
+
+			err = json.Unmarshal(requestBuf, unmountRequestWrapped)
+			if nil != err {
+				panic(fmt.Errorf("json.Unmarshal(requestBuf, unmountRequestWrapped) failed: %v", err))
+			}
+
+			unmountRequest = unmountRequestWrapped.Request
+			unmountReply = &Reply{}
+
+			err = jserver.RpcUnmount(unmountRequest, unmountReply)
+
+			unmountReplyWrapped = &benchmarkRpcShortcutUnmountReplyStruct{
+				Err:   err,
+				Reply: unmountReply,
+			}
+
+			replyBuf, err = json.Marshal(unmountReplyWrapped)
+			if nil != err {
+				panic(fmt.Errorf("json.Marshal(unmountReplyWrapped) failed"))
+			}
+		default:
+			panic(fmt.Errorf("benchmarkRpcShortcutRequestMethodOnly.Method (\"%s\") not recognized", benchmarkRpcShortcutRequestMethodOnly.Method))
+		}
+
+		replyLen = uint32(len(replyBuf))
+		replyLenBuf = make([]byte, 4)
+		binary.LittleEndian.PutUint32(replyLenBuf, replyLen)
+
+		n, err = netConn.Write(replyLenBuf)
+		if nil != err {
+			panic(fmt.Errorf("netConn.Write(replyLenBuf) failed: %v", err))
+		}
+		if n != 4 {
+			panic(fmt.Errorf("netConn.Write(replyLenBuf) returned n == %v (expected 4)", n))
+		}
+
+		n, err = netConn.Write(replyBuf)
+		if nil != err {
+			panic(fmt.Errorf("netConn.Write(replyBuf) failed: %v", err))
+		}
+		if n != int(replyLen) {
+			panic(fmt.Errorf("netConn.Write(replyBuf) returned n == %v (expected %v)", n, replyLen))
+		}
+	}
+}
+
+func benchmarkRpcLeaseShortcutDoRequest(b *testing.B, netConn net.Conn, request interface{}, reply interface{}) {
+	var (
+		err           error
+		n             int
+		replyBuf      []byte
+		replyLen      uint32
+		replyLenBuf   []byte
+		requestBuf    []byte
+		requestLen    uint32
+		requestLenBuf []byte
+	)
+
+	requestBuf, err = json.Marshal(request)
+	if nil != err {
+		b.Fatalf("json.Marshal(request) failed: %v", err)
+	}
+
+	requestLen = uint32(len(requestBuf))
+	requestLenBuf = make([]byte, 4)
+	binary.LittleEndian.PutUint32(requestLenBuf, requestLen)
+
+	n, err = netConn.Write(requestLenBuf)
+	if nil != err {
+		b.Fatalf("netConn.Write(requestLenBuf) failed: %v", err)
+	}
+	if n != 4 {
+		b.Fatalf("netConn.Write(requestLenBuf) returned n == %v (expected 4)", n)
+	}
+
+	n, err = netConn.Write(requestBuf)
+	if nil != err {
+		b.Fatalf("netConn.Write(requestBuf) failed: %v", err)
+	}
+	if n != int(requestLen) {
+		b.Fatalf("netConn.Write(requestBuf) returned n == %v (expected %v)", n, requestLen)
+	}
+
+	replyLenBuf = make([]byte, 4)
+
+	n, err = netConn.Read(replyLenBuf)
+	if nil != err {
+		b.Fatalf("netConn.Read(replyLenBuf) failed: %v", err)
+	}
+	if n != 4 {
+		b.Fatalf("netConn.Read(replyLenBuf) returned n == %v (expected 4)", n)
+	}
+
+	replyLen = binary.LittleEndian.Uint32(replyLenBuf)
+	replyBuf = make([]byte, replyLen)
+
+	n, err = netConn.Read(replyBuf)
+	if nil != err {
+		b.Fatalf("netConn.Read(replyBuf) failed: %v", err)
+	}
+	if n != int(replyLen) {
+		b.Fatalf("netConn.Read(replyBuf) returned n == %v (expected %v)", n, replyLen)
+	}
+
+	err = json.Unmarshal(replyBuf, reply)
+	if nil != err {
+		b.Fatalf("json.Unmarshal(replyBuf, reply) failed: %v", err)
+	}
+}
+
 func BenchmarkRpcLeaseLocal(b *testing.B) {
 	var (
 		benchmarkIteration        int
@@ -53,8 +507,8 @@ func BenchmarkRpcLeaseLocal(b *testing.B) {
 		jserver                   *Server
 		leaseReply                *LeaseReply
 		leaseRequest              *LeaseRequest
-		mountByAccountNameRequest *MountByAccountNameRequest
 		mountByAccountNameReply   *MountByAccountNameReply
+		mountByAccountNameRequest *MountByAccountNameRequest
 		unmountReply              *Reply
 		unmountRequest            *UnmountRequest
 	)
@@ -133,8 +587,8 @@ func BenchmarkRpcLeaseRemote(b *testing.B) {
 		keepAlivePeriod           time.Duration
 		leaseReply                *LeaseReply
 		leaseRequest              *LeaseRequest
-		mountByAccountNameRequest *MountByAccountNameRequest
 		mountByAccountNameReply   *MountByAccountNameReply
+		mountByAccountNameRequest *MountByAccountNameRequest
 		retryRPCClient            *retryrpc.Client
 		retryrpcClientConfig      *retryrpc.ClientConfig
 		testRpcLeaseClient        *testRpcLeaseClientStruct
