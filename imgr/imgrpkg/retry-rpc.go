8dd205decbed098549d74c99a176ec26b25d7b80
Ed McClanahan <emcclanahan@nvidia.com>
2021-04-05 17:42:30 -0700

    Trying to launch imgr's retryrpc listener... hangs on Close()
    A few UNDOs in imgr should be removed...

--- imgr/imgrpkg/retry-rpc.go
+++ imgr/imgrpkg/retry-rpc.go
@@ -3,6 +3,63 @@
 
 package imgrpkg
 
+import (
+	"crypto/tls"
+	"fmt"
+
+	"github.com/NVIDIA/proxyfs/retryrpc"
+)
+
+func startRetryRPCServer() (err error) {
+	var (
+		retryrpcServerConfig *retryrpc.ServerConfig
+		tlsCertificate       tls.Certificate
+	)
+
+	if "" == globals.config.RetryRPCCertFilePath { // && "" == globals.config.RetryRPCKeyFilePath
+		tlsCertificate = tls.Certificate{}
+	} else { // ("" != globals.config.RetryRPCCertFilePath) && ("" != globals.config.RetryRPCKeyFilePath)
+		tlsCertificate, err = tls.LoadX509KeyPair(globals.config.RetryRPCCertFilePath, globals.config.RetryRPCKeyFilePath)
+		if nil != err {
+			return
+		}
+	}
+
+	retryrpcServerConfig = &retryrpc.ServerConfig{
+		LongTrim:        globals.config.RetryRPCTTLCompleted,
+		ShortTrim:       globals.config.RetryRPCAckTrim,
+		IPAddr:          globals.config.PrivateIPAddr,
+		Port:            int(globals.config.RetryRPCPort),
+		DeadlineIO:      globals.config.RetryRPCDeadlineIO,
+		KeepAlivePeriod: globals.config.RetryRPCKeepAlivePeriod,
+		TLSCertificate:  tlsCertificate,
+	}
+
+	globals.retryrpcServer = retryrpc.NewServer(retryrpcServerConfig)
+
+	retryRPCServer = &RetryRPCServerStruct{}
+
+	err = globals.retryrpcServer.Register(retryRPCServer)
+	if nil != err {
+		return
+	}
+
+	err = globals.retryrpcServer.Start()
+
+	return nil // err set by globals.retryrpcServer.Start() is sufficient
+}
+
+func stopRetryRPCServer() (err error) {
+	fmt.Println("UNDO: stopRetryRPCServer() about to call globals.retryrpcServer.Close()")
+	globals.retryrpcServer.Close()
+	fmt.Println("UNDO: stopRetryRPCServer() back from call toglobals.retryrpcServer.Close()")
+
+	retryRPCServer = nil
+
+	err = nil
+	return nil
+}
+
 func mount(retryRPCClientID uint64, mountRequest *MountRequestStruct, mountReply *MountReplyStruct) (err error) {
 	return nil // TODO
 }
