3e4af98174037cb2ecb1657932a1f03286aa568d
Gerard Giné <ggine@nvidia.com>
2021-09-22 11:18:28 -0700

    CI builds won't fail if we can't post coverage results to Coveralls

--- test/container/launch.sh
+++ test/container/launch.sh
@@ -11,5 +11,5 @@ cd $GOPATH/src/github.com/NVIDIA/proxyfs
 make ci
 # $COVERALLS_TOKEN must be configured in the CI/CD environment
 if [ -n "$COVERALLS_TOKEN" ] && [ -n "$GIT_BRANCH" ]; then
-    goveralls -coverprofile coverage.coverprofile -service travis-ci -repotoken $COVERALLS_TOKEN
+    goveralls -coverprofile coverage.coverprofile -service travis-ci -repotoken $COVERALLS_TOKEN || true
 fi
